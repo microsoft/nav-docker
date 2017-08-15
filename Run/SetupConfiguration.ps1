14c291d16e1deb9165511c67898a406895dce958
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2017-08-15 19:19:41 +0200

    Fix UrlAcl reservation

--- Run/SetupConfiguration.ps1
+++ Run/SetupConfiguration.ps1
@@ -40,11 +40,18 @@ if ($developerServicesKeyExists) {
 }
 $CustomConfig.Save($CustomConfigFile)
 
-if ($servicesUseSSL) {
-    7045..7049 | % {
-        netsh http add urlacl url=$protocol+:$_/NAV user="NT AUTHORITY\SYSTEM" | Out-Null
+7045,7047,7048,7049 | % {
+    netsh http add urlacl url=$protocol+:$_/NAV user="NT AUTHORITY\SYSTEM" | Out-Null
+    if ($servicesUseSSL) {
         netsh http add sslcert ipport=0.0.0.0:$_ certhash=$certificateThumbprint appid="{00112233-4455-6677-8899-AABBCCDDEEFF}" | Out-Null
     }
+}
+
+if ($navUseSSL) {
+    netsh http add urlacl url=https://+:7046/NAV user="NT AUTHORITY\SYSTEM" | Out-Null
+    netsh http add sslcert ipport=0.0.0.0:7046 certhash=$certificateThumbprint appid="{00112233-4455-6677-8899-AABBCCDDEEFF}" | Out-Null
+} else {
+    netsh http add urlacl url=http://+:7046/NAV user="NT AUTHORITY\SYSTEM" | Out-Null
 }
 
 if ($developerServicesKeyExists) {
