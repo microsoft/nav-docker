62679b8eb94d16d2693c10f0da07fb7bc2a18aa3
freddydk <freddy.kristiansen@microsoft.com>
2017-12-04 15:25:33 +0100

    Display username

--- Run/SetupWindowsUsers.ps1
+++ Run/SetupWindowsUsers.ps1
@@ -7,7 +7,7 @@
 #
 
 if (($securePassword) -and $username -ne "") { 
-    Write-Host "Creating Windows user"
+    Write-Host "Creating Windows user $username"
     New-LocalUser -AccountNeverExpires -FullName $username -Name $username -Password $securePassword -ErrorAction Ignore | Out-Null
     Add-LocalGroupMember -Group administrators -Member $username -ErrorAction Ignore
 }
