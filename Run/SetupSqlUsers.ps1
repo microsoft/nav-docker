44bad3339c57ae4043b1692123b21ece399a18e7
freddydk <freddy.kristiansen@microsoft.com>
2018-02-26 21:55:14 +0100

    Create nav admin user as sysadmin in sql (for CSIDE)

--- Run/SetupSqlUsers.ps1
+++ Run/SetupSqlUsers.ps1
@@ -8,26 +8,43 @@
 #
 
 if ($securePassword) {
-    Write-Host "Enabling SA"
+    Write-Host "Setting SA Password and enabling SA"
     $sqlcmd = "ALTER LOGIN sa with password='" + ([System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecurePassword)).Replace('"','""').Replace('''','''''')) + "',CHECK_POLICY = OFF;ALTER LOGIN sa ENABLE;"
     & sqlcmd -S "$databaseServer\$databaseInstance" -Q $sqlcmd
-}
 
-if ($auth -eq "Windows" -and $username -ne "" -and (!($securePassword))) {
-    Write-Host "Adding $username to sysadmin"
-    $sqlcmd = 
-        "IF NOT EXISTS 
-            (SELECT name  
-            FROM master.sys.server_principals
-            WHERE name = '$username')
-        BEGIN
-            CREATE LOGIN [$username] FROM WINDOWS
-            EXEC sp_addsrvrolemember '$username', 'sysadmin'
-        END
-        
-        ALTER LOGIN [$username] ENABLE
-        GO"
-        
-    & sqlcmd -S "$databaseServer\$databaseInstance" -Q $sqlcmd
-}
+	if ($auth -ne "Windows" -and $username -ne "") {
+		Write-Host "Creating $username as SQL User and add to sysadmin"
+		$sqlcmd = 
+			"IF NOT EXISTS 
+				(SELECT name  
+				FROM master.sys.server_principals
+				WHERE name = '$username')
+			BEGIN
+				CREATE LOGIN $username with password='" + ([System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecurePassword)).Replace('"','""').Replace('''','''''')) + "',CHECK_POLICY = OFF
+				EXEC sp_addsrvrolemember '$username', 'sysadmin'
+			END
 
+			ALTER LOGIN [$username] ENABLE
+			GO"
+			
+		& sqlcmd -S "$databaseServer\$databaseInstance" -Q $sqlcmd
+	}
+} else {
+	if ($auth -eq "Windows" -and $username -ne "") {
+		Write-Host "Adding $username to sysadmin"
+		$sqlcmd = 
+			"IF NOT EXISTS 
+				(SELECT name  
+				FROM master.sys.server_principals
+				WHERE name = '$username')
+			BEGIN
+				CREATE LOGIN [$username] FROM WINDOWS
+				EXEC sp_addsrvrolemember '$username', 'sysadmin'
+			END
+			
+			ALTER LOGIN [$username] ENABLE
+			GO"
+			
+		& sqlcmd -S "$databaseServer\$databaseInstance" -Q $sqlcmd
+	}
+}
