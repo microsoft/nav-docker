ba722713bb247bcab049aafbedd033d762e5d988
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2018-06-02 13:23:32 +0200

    Add build script and remove dockerfile

---
+++ build.ps1
@@ -0,0 +1,11 @@
+﻿. (Join-Path $PSScriptRoot "settings.ps1")
+. (Join-Path $PSScriptRoot "BuildAndPushGeneric.ps1")
+
+BuildAndPushGeneric -registry $registry `
+                    -imageFolder $PSScriptRoot `
+                    -windowsservercoreImage "microsoft/dotnet-framework:4.7.2-sdk-20180523-windowsservercore-ltsc2016" `
+                    -tag $tag `
+                    -silent $silent `
+                    -removeImage $false `
+                    -latest $latest `
+                    -forceRebuild $true
---
+++ settings.ps1
@@ -0,0 +1,6 @@
+﻿$registry = "microsoft"
+$removeImage = $false
+$silent = $false
+$latest = $true
+$tag = "0.0.5.6"
+$tag = "0.0.6.0"
