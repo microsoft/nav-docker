c4a208d16c80c2d86e840925e8793480a942f2dc
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2018-10-12 11:05:59 +0200

    Use AZ ACR to build

--- build.ps1
+++ build.ps1
@@ -1,38 +1,24 @@
-﻿$registry = "microsoft"
-$tag = "0.0.6.5"
+﻿$registry = "" #"microsoft"
+$tag = "0.0.7.0"
 $latest = $false
 $removeImage = $false
+$created = [DateTime]::Now.ToUniversalTime().ToString("yyyyMMddHHmm")
 
-"1803","1709" | % {
-#"ltsc2016" | % {
+"ltsc2016","1803","1709" | ForEach-Object {
 
     $baseVersionTag = $_
-    $baseImage = "microsoft/dotnet-framework:4.7.2-runtime-windowsservercore-$baseVersionTag"
-    
-    $maintainer = "Dynamics SMB"
-    $eula = "https://go.microsoft.com/fwlink/?linkid=861843"
-    
-    docker pull $baseImage
-    $osversion = docker inspect --format "{{.OsVersion}}" $baseImage
-    $created = [DateTime]::Now.ToUniversalTime().ToString("yyyyMMddHHmm")
-        
     $imageNameTag = "dynamics-nav:generic"
     $buildImageNameTag = "$imageNameTag-$baseVersionTag"
     Write-Host -ForegroundColor Yellow "Build $buildImageNameTag"
 
     $imageFolder = $PSScriptRoot
     $dockerFilePath = Join-Path $imageFolder "DOCKERFILE"
-    $dockerFile = Get-Content $dockerFilePath
-    $dockerFile[0] = "FROM $baseImage"
-    $dockerFile | Set-Content $dockerFilePath
         
     docker rmi $buildImageNameTag -f 2>NULL | Out-Null
-    docker build --label maintainer="$maintainer" `
-                 --label created="$created" `
-                 --label tag="$tag" `
-                 --label osversion="$osversion" `
-                 --label eula="$eula" `
-                 -t $buildImageNameTag `
+    docker build --build-arg created="$created" `
+                 --build-arg tag="$tag" `
+                 --tag $buildImageNameTag `
+                 --file $dockerFilePath
                  $imageFolder
     if ($LastExitCode -ne 0) { throw "Docker build error" }
     
