1767a44b354e72d89b08cf072d4890b9680032d6
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2018-10-16 06:15:09 +0200

    update

--- generic/build-acr.ps1
+++ generic/build-acr.ps1
@@ -1,29 +1,33 @@
 ﻿. (Join-Path $PSScriptRoot "Settings.ps1")
+
 $acr = "navgeneric"
 
-#az acr run --registry $acr --file build-and-push.yaml --set created=$created --set tag=$tag --os Windows $PSScriptRoot
+#"1803","1709" | ForEach-Object {
+"1709" | ForEach-Object {
 
-"1803","1709","ltsc2016" | ForEach-Object {
-    
     if ($_ -eq "ltsc2019") {
         $baseimage = "mcr.microsoft.com/windows/servercore:$_"
     } else {
         $baseimage = "microsoft/dotnet-framework:4.7.2-runtime-windowsservercore-$_"
     }
 
+    if ($_ -eq "ltsc2016") {
+        throw "ltsc2016 cannot be build on ACR"
+    }
+
     $image = "generic:$_"
 
-    az acr build --registry $acr `
-                 --image $image `
-                 --timeout 7200 `
-                 --os Windows `
-                 --build-arg baseimage=$baseimage `
-                 --build-arg created=$created `
-                 --build-arg tag=$tag `
-                 --file DOCKERFILE `
-                 https://github.com/Microsoft/nav-docker.git#master:generic
+#    az acr build --registry $acr `
+#                 --image $image `
+#                 --timeout 7200 `
+#                 --os Windows `
+#                 --build-arg baseimage=$baseimage `
+#                 --build-arg created=$created `
+#                 --build-arg tag=$tag `
+#                 --file DOCKERFILE `
+#                 https://github.com/Microsoft/nav-docker.git#master:generic
     
-    if ($LASTEXITCODE -eq 0) {
+  #  if ($LASTEXITCODE -eq 0) {
         Write-Host "SUCCESS"
 
         $tags = @("microsoft/dynamics-nav:generic-$_")
@@ -33,5 +37,5 @@ $acr = "navgeneric"
             docker tag "$acr.azurecr.io/$image" $_
             docker push $_
         }
-    }
+   # }
 }
