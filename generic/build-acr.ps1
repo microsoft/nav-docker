00f57ffe47ca3f7cefa30834d1d60d26639b1c55
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2018-10-17 07:20:41 +0200

    update build scripts

--- generic/build-acr.ps1
+++ generic/build-acr.ps1
@@ -1,17 +1,26 @@
-﻿. (Join-Path $PSScriptRoot "Settings.ps1")
-$acr = "navgeneric"
+﻿Param(
+    [string] $acr = "navgeneric",
+    [string[]] $oss = @("1803","1709")
+)
 
-#az acr run --registry $acr --file build-and-push.yaml --set created=$created --set tag=$tag --os Windows $PSScriptRoot
+. (Join-Path $PSScriptRoot "Settings.ps1")
 
-"1803","1709","ltsc2016" | ForEach-Object {
-    
+$oss | ForEach-Object {
+
     if ($_ -eq "ltsc2019") {
         $baseimage = "mcr.microsoft.com/windows/servercore:$_"
     } else {
         $baseimage = "microsoft/dotnet-framework:4.7.2-runtime-windowsservercore-$_"
     }
 
+    if ($_ -eq "ltsc2016") {
+        throw "ltsc2016 cannot be build on ACR"
+    }
+
     $image = "generic:$_"
+
+    docker pull $baseimage
+    $osversion = docker inspect --format "{{.OsVersion}}" $baseImage
 
     az acr build --registry $acr `
                  --image $image `
@@ -20,6 +29,7 @@ $acr = "navgeneric"
                  --build-arg baseimage=$baseimage `
                  --build-arg created=$created `
                  --build-arg tag=$tag `
+                 --build-arg osversion=$osversion `
                  --file DOCKERFILE `
                  https://github.com/Microsoft/nav-docker.git#master:generic
     
@@ -27,6 +37,9 @@ $acr = "navgeneric"
         Write-Host "SUCCESS"
 
         $tags = @("microsoft/dynamics-nav:generic-$_")
+        if ($_ -eq "ltsc2016") {
+            $tags += "microsoft/dynamics-nav:generic"
+        }
     
         docker pull "$acr.azurecr.io/$image"
         $tags | ForEach-Object {
--- generic/build-local.ps1
+++ generic/build-local.ps1
@@ -1,6 +1,11 @@
-﻿. (Join-Path $PSScriptRoot "Settings.ps1")
+﻿Param(
+    [string] $acr = "navgeneric",
+    [string[]] $oss = @("ltsc2016")
+)
 
-"1803","1709","ltsc2016" | ForEach-Object {
+. (Join-Path $PSScriptRoot "Settings.ps1")
+
+$oss | ForEach-Object {
     
     if ($_ -eq "ltsc2019") {
         $baseimage = "mcr.microsoft.com/windows/servercore:$_"
@@ -8,14 +13,22 @@
         $baseimage = "microsoft/dotnet-framework:4.7.2-runtime-windowsservercore-$_"
     }
 
-    docker pull $baseimage
+    if ($_ -eq "ltsc2016") {
+        if ([System.Environment]::OSVersion.Version.Build -ne 14393) {
+            throw "ltsc2016 cannot be build on host OS other than ltsc2016"
+        }
+    }
+
     $image = "generic:$_"
+
+    docker pull $baseimage
+    $osversion = docker inspect --format "{{.OsVersion}}" $baseImage
 
     docker rmi $image -f 2>NULL | Out-Null
     docker build --build-arg baseimage=$baseimage `
                  --build-arg created=$created `
                  --build-arg tag=$tag `
-                 --isolation hyperv `
+                 --build-arg osversion="$osversion" `
                  --tag $image `
                  $PSScriptRoot
     
@@ -23,6 +36,9 @@
         Write-Host "SUCCESS"
 
         $tags = @("microsoft/dynamics-nav:generic-$_")
+        if ($_ -eq "ltsc2016") {
+            $tags += "microsoft/dynamics-nav:generic"
+        }
 
         $tags | ForEach-Object {
             docker tag $image $_
