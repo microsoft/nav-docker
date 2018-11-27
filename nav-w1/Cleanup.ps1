331bb4847eae8dad17701b4b0150f5b8cb9b413c
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2018-11-27 07:13:12 +0100

    add cleanup

---
+++ nav-w1/Cleanup.ps1
@@ -0,0 +1,14 @@
+﻿Param( 
+    $context,
+    [PSCustomObject] $json )
+
+# Json usage:
+#
+# $json = '{
+#     "blobcontainer":  "<blobcontainer>",
+#     "navdvdblobname":  "<blobname>",
+#     "vsixblobname":  "<blobname>",
+# }' | ConvertFrom-Json
+
+Remove-AzureStorageBlob -Context $context -Container $json.blobcontainer -Blob $json.navdvdblobname -Force -ErrorAction SilentlyContinue
+Remove-AzureStorageBlob -Context $context -Container $json.blobcontainer -Blob $json.vsixblobname -Force -ErrorAction SilentlyContinue
