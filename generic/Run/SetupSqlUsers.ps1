2e7974541144cd3e3080f1072367312f5f0cfe36
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2019-03-22 05:33:19 -0700

    Fix for bug 370 in NavContainerHelper issues

--- generic/Run/SetupSqlUsers.ps1
+++ generic/Run/SetupSqlUsers.ps1
@@ -20,7 +20,7 @@ if ($securePassword) {
 				FROM master.sys.server_principals
 				WHERE name = '$username')
 			BEGIN
-				CREATE LOGIN $username with password='" + ([System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecurePassword)).Replace('"','""').Replace('''','''''')) + "',CHECK_POLICY = OFF
+				CREATE LOGIN [$username] with password='" + ([System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecurePassword)).Replace('"','""').Replace('''','''''')) + "',CHECK_POLICY = OFF
 				EXEC sp_addsrvrolemember '$username', 'sysadmin'
 			END
 
