969f3fd38c3ef5fbce4cd175ec51bdffbbe9cc77
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2019-04-12 00:27:44 +0200

    Create SetupDatabase.ps1

---
+++ override/attachdb/SetupDatabase.ps1
@@ -0,0 +1,14 @@
+﻿if ($restartingInstance) {
+    # Nothing to do
+}
+else {
+    $dbpath = "c:\mydb"
+    $mdf = (Get-Item (Join-Path $dbpath "*.mdf")).FullName
+    $ldf = (Get-Item (Join-Path $dbpath "*.ldf")).FullName
+    $databaseName = "MYCRONUS"
+    $databaseServer = "localhost"
+    $databaseInstance = "SQLEXPRESS"
+    $attachcmd = "USE [master] CREATE DATABASE [$DatabaseName] ON (FILENAME = '$mdf'),(FILENAME = '$ldf') FOR ATTACH"
+    Write-Host "Attaching database in $mdf/$ldf as $DatabaseName"
+    Invoke-Sqlcmd -ServerInstance localhost\SQLEXPRESS -QueryTimeOut 0 -ea Stop -Query $attachcmd
+}
