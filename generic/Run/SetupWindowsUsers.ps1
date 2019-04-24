2d9c99938a4cb7bc8adddc515a1d9764fa505d52
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2019-04-24 15:14:20 +0200

    Issue #434

--- generic/Run/SetupWindowsUsers.ps1
+++ generic/Run/SetupWindowsUsers.ps1
@@ -6,9 +6,11 @@
 # OUTPUT
 #
 
-if (($securePassword) -and $username -ne "") { 
-    Write-Host "Creating Windows user $username"
-    New-LocalUser -AccountNeverExpires -FullName $username -Name $username -Password $securePassword | Out-Null
-    Add-LocalGroupMember -Group administrators -Member $username
+if ($auth -eq "Windows") {
+    if (($securePassword) -and $username -ne "") { 
+        Write-Host "Creating Windows user $username"
+        New-LocalUser -AccountNeverExpires -FullName $username -Name $username -Password $securePassword | Out-Null
+        Add-LocalGroupMember -Group administrators -Member $username
+    }
 }
 
