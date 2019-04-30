b7cd485f68999081e9e65686639b9af6c766d56e
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2019-04-30 19:52:08 +0200

    Private build of generic images

---
+++ generic/mybuild.ps1
@@ -0,0 +1,27 @@
+﻿$baseimage = "mcr.microsoft.com/windows/servercore/insider:10.0.18362.53"
+$isolation = "process"
+$image = "mygeneric"
+$genericVersion = "0.0.9.6"
+
+docker pull $baseimage
+$osversion = docker inspect --format "{{.OsVersion}}" $baseImage
+
+docker images --format "{{.Repository}}:{{.Tag}}" | % { 
+    if ($_ -eq $image) 
+    {
+        docker rmi $image -f
+    }
+}
+
+docker build --build-arg baseimage=$baseimage `
+             --build-arg created=$created `
+             --build-arg tag="$genericVersion" `
+             --build-arg osversion="$osversion" `
+             --isolation=$isolation `
+             --tag $image `
+             $PSScriptRoot
+
+if ($LASTEXITCODE -ne 0) {
+    throw "Failed with exit code $LastExitCode"
+}
+Write-Host "SUCCESS"
