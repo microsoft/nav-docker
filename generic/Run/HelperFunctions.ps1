48cf1d617d2419fff235497341c24110aa36826e
Freddy Kristiansen <freddyk@microsoft.com>
2020-06-13 18:21:55 +0200

    Include Download-FIle

--- generic/Run/HelperFunctions.ps1
+++ generic/Run/HelperFunctions.ps1
@@ -597,6 +597,30 @@ function SetDatabaseServerCollation {
     }
 }
 
+function Download-File {
+    Param (
+        [Parameter(Mandatory=$true)]
+        [string] $sourceUrl,
+        [Parameter(Mandatory=$true)]
+        [string] $destinationFile,
+        [switch] $dontOverwrite
+    )
+
+    if (Test-Path $destinationFile -PathType Leaf) {
+        if ($dontOverwrite) { 
+            return
+        }
+        Remove-Item -Path $destinationFile -Force
+    }
+    $path = [System.IO.Path]::GetDirectoryName($destinationFile)
+    if (!(Test-Path $path -PathType Container)) {
+        New-Item -Path $path -ItemType Directory -Force | Out-Null
+    }
+    [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+    Write-Host "Downloading $destinationFile"
+    (New-Object System.Net.WebClient).DownloadFile($sourceUrl, $destinationFile)
+}
+
 function Download-Artifacts {
     Param (
         [Parameter(Mandatory=$true)]
