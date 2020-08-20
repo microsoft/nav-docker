771db52b45484c2b86151d5c73037017780e2d62
Freddy Kristiansen <freddyk@microsoft.com>
2020-08-20 09:05:06 +0200

    Add alternate id to default tenant

--- generic/Run/SetupTenant.ps1
+++ generic/Run/SetupTenant.ps1
@@ -1,14 +1,19 @@
-﻿if ($restartingInstance) {
-
-    # Nothing to do
-
-} elseif ($databaseServer -eq "localhost" -and $databaseInstance -eq "SQLEXPRESS") {
+﻿if ($newPublicDnsName -and $databaseServer -eq "localhost" -and $databaseInstance -eq "SQLEXPRESS") {
 
     if (!(Test-NavDatabase -DatabaseName $TenantId)) {
-        # Setup tenant
+        Write-Host "Copying template database"
         Copy-NavDatabase -SourceDatabaseName "tenant" -DestinationDatabaseName $TenantId
-        Mount-NavDatabase -ServerInstance $ServerInstance -TenantId $TenantId -DatabaseName $TenantId
     }
+    else {
+        Write-Host "Dismounting Tenant"
+        Dismount-NavTenant -ServerInstance $ServerInstance -Tenant $TenantId -Force | Out-Null
+    }
+    $alternateId = @($hostname, "$hostname-$TenantId")
+    if ($publicDnsName -ne $hostname) {
+        $alternateId += @($publicDnsName)
+    }
+    Write-Host "Mounting Tenant"
+    Mount-NavDatabase -ServerInstance $ServerInstance -TenantId $TenantId -DatabaseName $TenantId -AlternateId $alternateId
     $tenantStartTime = [DateTime]::Now
     while ([DateTime]::Now.Subtract($tenantStartTime).TotalSeconds -le 60) {
         $tenantInfo = Get-NAVTenant -ServerInstance $ServerInstance -Tenant $TenantId
