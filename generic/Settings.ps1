573359a13e7ac57f5c42b1aeca882a39ee55932b
freddyk <freddyk@microsoft.com>
2021-02-28 07:50:53 +0100

    update scripts

--- generic/Settings.ps1
+++ generic/Settings.ps1
@@ -1,2 +1,2 @@
-﻿$genericTag = "1.0.1.3"
+﻿$genericTag = "1.0.1.4"
 $created = [DateTime]::Now.ToUniversalTime().ToString("yyyyMMddHHmm")
--- generic/buildupdate.ps1
+++ generic/buildupdate.ps1
@@ -16,7 +16,7 @@ $testImages = $false
 
 $pushto = @()
 $pushto = @("dev")
-#$pushto += @("prod")
+$pushto = @("prod")
 
 $ver = [Version]"10.0.0.0"
 $tags = (get-navcontainerimagetags -imageName "mcr.microsoft.com/businesscentral").tags | 
@@ -48,17 +48,22 @@ $tags | % {
 
     docker pull $baseimage
 
-    $image = "my:$osversion-$genericTag"
-
-    docker images --format "{{.Repository}}:{{.Tag}}" | % { 
-        if ($_ -eq $image) 
-        {
-            docker rmi $image -f
-        }
+    $inspect = docker inspect $baseimage | ConvertFrom-Json
+    if ([version]$inspect.config.Labels.tag -ge [version]$generictag) {
+        Write-Host -ForegroundColor Green "Image already built"
     }
+    else {
+        $image = "my:$osversion-$genericTag"
     
-    $dockerfile = Join-Path $RootPath "DOCKERFILE.UPDATE"
-
+        docker images --format "{{.Repository}}:{{.Tag}}" | % { 
+            if ($_ -eq $image) 
+            {
+                docker rmi $image -f
+            }
+        }
+        
+        $dockerfile = Join-Path $RootPath "DOCKERFILE.UPDATE"
+    
 @"
 FROM $baseimage
 
@@ -67,63 +72,64 @@ COPY Run /Run/
 LABEL tag="$genericTag" \
       created="$created"
 "@ | Set-Content $dockerfile
-
-    docker build --isolation=$isolation `
-                 --tag $image `
-                 --file $dockerfile `
-                 --memory 4G `
-                 $RootPath
-
-    if ($LASTEXITCODE -ne 0) {
-        throw "Failed with exit code $LastExitCode"
-    }
-    Write-Host "SUCCESS"
-    Remove-Item $dockerfile -Force
-
-    # Test image
-    if ($testImages) {
-        $artifactUrl = Get-BCArtifactUrl -type OnPrem -country w1
-        $password = 'P@ssword1'
-        $securePassword = ConvertTo-SecureString -String $password -AsPlainText -Force
-        $credential = New-Object pscredential 'admin', $securePassword
-        
-        $parameters = @{
-            "accept_eula"               = $true
-            "containerName"             = "test"
-            "artifactUrl"               = $artifactUrl
-            "useGenericImage"           = $image
-            "auth"                      = "NAVUserPassword"
-            "Credential"                = $credential
-            "updateHosts"               = $true
-            "doNotCheckHealth"          = $true
-            "EnableTaskScheduler"       = $false
-            "Isolation"                 = "hyperv"
-            "MemoryLimit"               = "8G"
+    
+        docker build --isolation=$isolation `
+                     --tag $image `
+                     --file $dockerfile `
+                     --memory 4G `
+                     $RootPath
+    
+        if ($LASTEXITCODE -ne 0) {
+            throw "Failed with exit code $LastExitCode"
         }
-        
-        New-NavContainer @parameters
-        Remove-NavContainer -containerName "test"
+        Write-Host "SUCCESS"
+        Remove-Item $dockerfile -Force
+    
+        # Test image
+        if ($testImages) {
+            $artifactUrl = Get-BCArtifactUrl -type OnPrem -country w1
+            $password = 'P@ssword1'
+            $securePassword = ConvertTo-SecureString -String $password -AsPlainText -Force
+            $credential = New-Object pscredential 'admin', $securePassword
+            
+            $parameters = @{
+                "accept_eula"               = $true
+                "containerName"             = "test"
+                "artifactUrl"               = $artifactUrl
+                "useGenericImage"           = $image
+                "auth"                      = "NAVUserPassword"
+                "Credential"                = $credential
+                "updateHosts"               = $true
+                "doNotCheckHealth"          = $true
+                "EnableTaskScheduler"       = $false
+                "Isolation"                 = "hyperv"
+                "MemoryLimit"               = "8G"
+            }
+            
+            New-NavContainer @parameters
+            Remove-NavContainer -containerName "test"
+        }
+    
+        $newtags = @()
+        if ($pushto.Contains("dev")) {
+            $newtags += @(
+                "mcrbusinesscentral.azurecr.io/public/businesscentral:$osversion-dev"
+            )
+        }
+        if ($pushto.Contains("prod")) {
+            $newtags += @(
+                "mcrbusinesscentral.azurecr.io/public/businesscentral:$osversion",
+                "mcrbusinesscentral.azurecr.io/public/businesscentral:$osversion-$genericTag")
+        }
+        $newtags | ForEach-Object {
+            Write-Host "Push $_"
+            docker tag $image $_
+            docker push $_
+        }
+        $newtags | ForEach-Object {
+            Write-Host "Remove $_"
+            docker rmi $_
+        }
+        docker rmi $image
     }
-
-    $newtags = @()
-    if ($pushto.Contains("dev")) {
-        $newtags += @(
-            "mcrbusinesscentral.azurecr.io/public/businesscentral:$osversion-dev"
-        )
-    }
-    if ($pushto.Contains("prod")) {
-        $newtags += @(
-            "mcrbusinesscentral.azurecr.io/public/businesscentral:$osversion",
-            "mcrbusinesscentral.azurecr.io/public/businesscentral:$osversion-$genericTag")
-    }
-    $newtags | ForEach-Object {
-        Write-Host "Push $_"
-        docker tag $image $_
-        docker push $_
-    }
-    $newtags | ForEach-Object {
-        Write-Host "Remove $_"
-        docker rmi $_
-    }
-    docker rmi $image
 }
