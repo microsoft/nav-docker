d8f57d18b14eaca617595a14e56c561992b4eebc
Freddy Kristiansen <freddyk@microsoft.com>
2021-10-11 22:21:06 +0200

    ignore missing add-ins folder

--- generic/Run/SetupAddIns.ps1
+++ generic/Run/SetupAddIns.ps1
@@ -9,7 +9,7 @@
 $AddinsFolder = (Join-Path $runPath "Add-ins")
 if (Test-Path $AddinsFolder -PathType Container) {
     copy-item -Path (Join-Path $AddinsFolder "*") -Destination (Join-Path $serviceTierFolder "Add-ins") -Recurse
-    if ($roleTailoredClientFolder) {
+    if ($roleTailoredClientFolder -and (Test-Path (Join-Path $roleTailoredClientFolder "Add-ins"))) {
         copy-item -Path (Join-Path $AddinsFolder "*") -Destination (Join-Path $roleTailoredClientFolder "Add-ins") -Recurse
     }
 }
