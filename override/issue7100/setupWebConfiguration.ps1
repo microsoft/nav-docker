614783b223a51350f3427be52b1c8d09233c42be
freddydk <freddyk@microsoft.com>
2022-12-05 22:17:11 +0100

    override

---
+++ override/issue7100/setupWebConfiguration.ps1
@@ -0,0 +1,6 @@
+﻿$wwwRootPath = Get-WWWRootPath
+$webConfigFile = Join-Path $wwwRootPath "$webServerInstance\web.config"
+$webconfig = Get-Content -Path $webConfigFile -Raw -Encoding UTF8
+$webConfig = $webconfig.Replace('<!-- <rewrite>','<rewrite>').Replace('</rewrite> -->','</rewrite>').Replace('{R:0}?tenant={C:1}','{R:0}?tenant=default')
+Set-Content -Path $webConfigFile -Value $webconfig -Encoding UTF8
+. 'C:\run\SetupWebConfiguration.ps1'
