a9d919ba6423540f2e289573375e1eb62de1039d
freddydk <freddydk@users.noreply.github.com>
2024-03-05 14:11:58 +0100

    include testapps in image

--- generic/Run/240/navinstall.ps1
+++ generic/Run/240/navinstall.ps1
@@ -341,55 +341,34 @@ if (!$skipDb -and ($multitenant -or $installOnly -or $licenseFilePath -ne "" -or
     }
 
     if ($installApps) {
-        $serviceTierFolder = (Get-Item "C:\Program Files\Microsoft Dynamics NAV\*\Service").FullName
-        $moduleImported = $false
-        if (Test-Path "$serviceTierFolder\Microsoft.Dynamics.Nav.Apps.Management.psd1") {
-            Import-Module "$serviceTierFolder\Microsoft.Dynamics.Nav.Apps.Management.psd1"
-            $moduleImported = $true
-        }
-        elseif (Test-Path "$serviceTierFolder\Management\Microsoft.Dynamics.Nav.Apps.Management.psd1") {
-            Import-Module "$serviceTierFolder\Management\Microsoft.Dynamics.Nav.Apps.Management.psd1"
-            $moduleImported = $true
-        }
-        else {
-            Write-Host "WARNING: Could not find Microsoft.Dynamics.Nav.Apps.Management.psd1"
-        }
-
-        if ($moduleImported) {
-            $installApps | ForEach-Object {
-                $appFile = $_
-    
-                $navAppInfo = Get-NAVAppInfo -Path $appFile
-                $appPublisher = $navAppInfo.Publisher
-                $appName = $navAppInfo.Name
-                $appVersion = $navAppInfo.Version
-
-                $syncAndInstall = $true
-                $tenantAppInfo = Get-NAVAppInfo -ServerInstance $serverInstance -Name $appName -Publisher $appPublisher -Version $appVersion -tenant default -tenantSpecificProperties
-                if ($tenantAppInfo) {
-                    if ($tenantAppInfo.IsInstalled) {
-                        Write-Host "Skipping $appName as it is already installed"
-                        $syncAndInstall = $false
-                    }
-                    else {
-                        Write-Host "$appName is already published"
-                    }
+        $installApps | ForEach-Object {
+            $appFile = $_
+            $navAppInfo = Get-NAVAppInfo -Path $appFile
+            $appPublisher = $navAppInfo.Publisher
+            $appName = $navAppInfo.Name
+            $appVersion = $navAppInfo.Version
+            $syncAndInstall = $true
+            $tenantAppInfo = Get-NAVAppInfo -ServerInstance $serverInstance -Name $appName -Publisher $appPublisher -Version $appVersion -tenant default -tenantSpecificProperties
+            if ($tenantAppInfo) {
+                if ($tenantAppInfo.IsInstalled) {
+                    Write-Host "Skipping $appName as it is already installed"
+                    $syncAndInstall = $false
                 }
                 else {
-                    Write-Host "Publishing $appFile"
-                    Publish-NavApp -ServerInstance $ServerInstance -Path $appFile -SkipVerification
-                }
-
-                if ($syncAndInstall) {    
-                    Write-Host "Synchronizing $appName"
-                    Sync-NavTenant -ServerInstance $ServerInstance -Tenant default -Force
-                    Sync-NavApp -ServerInstance $ServerInstance -Publisher $appPublisher -Name $appName -Version $appVersion -Tenant default -Mode ForceSync -force -WarningAction Ignore
-
-                    Write-Host "Installing $appName"
-                    Install-NavApp -ServerInstance $ServerInstance -Publisher $appPublisher -Name $appName -Version $appVersion -Tenant default
+                    Write-Host "$appName is already published"
                 }
             }
-
+            else {
+                Write-Host "Publishing $appFile"
+                Publish-NavApp -ServerInstance $ServerInstance -Path $appFile -SkipVerification
+            }
+            if ($syncAndInstall) {    
+                Write-Host "Synchronizing $appName"
+                Sync-NavTenant -ServerInstance $ServerInstance -Tenant default -Force
+                Sync-NavApp -ServerInstance $ServerInstance -Publisher $appPublisher -Name $appName -Version $appVersion -Tenant default -Mode ForceSync -force -WarningAction Ignore
+                Write-Host "Installing $appName"
+                Install-NavApp -ServerInstance $ServerInstance -Publisher $appPublisher -Name $appName -Version $appVersion -Tenant default
+            }
         }
     }
     
