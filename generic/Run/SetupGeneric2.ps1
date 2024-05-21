b9317566c5700be5dad15f13a295e0b00a604821
freddydk <freddydk@users.noreply.github.com>
2024-05-22 01:10:21 +0200

    no bc

--- generic/Run/SetupGeneric2.ps1
+++ generic/Run/SetupGeneric2.ps1
@@ -3,7 +3,7 @@ Write-Host "only24=$env:only24"
 $filesonly = $env:filesonly -eq 'true'
 $only24 = $env:only24 -eq 'true'
 if (-not $filesonly) {
-    Write-Host 'Installing SqlServer Module in PowerShell 7'; \
-    pwsh -Command 'Install-Module -Name SqlServer -RequiredVersion 22.2.0 -Scope AllUsers -Force'; \
+    Write-Host 'Installing SqlServer Module in PowerShell 7'
+    pwsh -Command 'Install-Module -Name SqlServer -RequiredVersion 22.2.0 -Scope AllUsers -Force'
     Write-Host 'Done'
 }
