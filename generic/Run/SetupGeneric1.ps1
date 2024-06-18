cb9f2862315c7759ce8a32d00fb6ef5cdc14bfee
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2024-06-18 17:04:42 +0200

    remove update yaml (#554)
    Co-authored-by: freddydk <freddydk@users.noreply.github.com>

--- generic/Run/SetupGeneric1.ps1
+++ generic/Run/SetupGeneric1.ps1
@@ -1,28 +1,4 @@
-
-$sql2019url = 'https://go.microsoft.com/fwlink/p/?linkid=866658'
-
-# https://learn.microsoft.com/en-us/troubleshoot/sql/releases/sqlserver-2019/build-versions
-$sql2019LatestCuUrl = 'https://download.microsoft.com/download/6/e/7/6e72dddf-dfa4-4889-bc3d-e5d3a0fd11ce/SQLServer2019-KB5035123-x64.exe'
-
-# https://dotnet.microsoft.com/en-us/download/dotnet/6.0 - grab the direct link behind ASP.NET Core Runtime Windows -> Hosting Bundle
-$dotNet6url = 'https://download.visualstudio.microsoft.com/download/pr/41643a5c-1ed5-41c8-abd0-473112282a79/644e14ace834d476fe3fa6797e472c55/dotnet-hosting-6.0.30-win.exe'
-
-# https://dotnet.microsoft.com/en-us/download/dotnet/8.0 - grab the direct link behind ASP.NET Core Runtime Windows -> Hosting Bundle
-$dotNet8url = 'https://download.visualstudio.microsoft.com/download/pr/70f96ebd-54ce-4bb2-a90f-2fbfc8fd90c0/aa542f2f158cc6c9e63b4287e4618f0a/dotnet-hosting-8.0.5-win.exe'
-
-# https://github.com/PowerShell/PowerShell/releases - grab the latest PowerShell-7.4.x-win-x64.msi link
-$powerShell7url = 'https://github.com/PowerShell/PowerShell/releases/download/v7.4.2/PowerShell-7.4.2-win-x64.msi'
-
-# Misc URLs
-$rewriteUrl = 'https://download.microsoft.com/download/1/2/8/128E2E22-C1B9-44A4-BE2A-5859ED1D4592/rewrite_amd64_en-US.msi'
-$sqlncliUrl = 'https://download.microsoft.com/download/B/E/D/BED73AAC-3C8A-43F5-AF4F-EB4FEA6C8F3A/ENU/x64/sqlncli.msi'
-$vcredist_x86url = 'https://aka.ms/highdpimfc2013x86enu'
-$vcredist_x64url = 'https://aka.ms/highdpimfc2013x64enu'
-$vsredist_x64_140url = 'https://aka.ms/vs/17/release/vc_redist.x64.exe'
-
-# NAV/BC Docker Install Files
-$navDockerInstallUrl = 'https://bcartifacts.blob.core.windows.net/prerequisites/nav-docker-install.zip'
-$openXmlSdkV25url = 'https://bcartifacts.blob.core.windows.net/prerequisites/OpenXMLSDKv25.msi'
+. (Join-Path $PSScriptRoot 'SetupUrls.ps1')
 
 Write-Host "FilesOnly=$env:filesOnly"
 Write-Host "only24=$env:only24"
@@ -140,7 +116,7 @@ if (-not $only24) {
     if (($null -ne $process.ExitCode) -and ($process.ExitCode -ne 0)) { Write-Host ('EXIT CODE '+$process.ExitCode) } else { Write-Host 'Success' }
 }
 Write-Host 'Downloading vcredist_x64_140'
-Invoke-RestMethod -Method Get -UseBasicParsing -Uri $vsredist_x64_140url -OutFile 'temp\vcredist_x64_140.exe'
+Invoke-RestMethod -Method Get -UseBasicParsing -Uri $vcredist_x64_140url -OutFile 'temp\vcredist_x64_140.exe'
 Write-Host 'Installing vcredist_x64_140'
 $process = start-process -Wait -FilePath 'temp\vcredist_x64_140.exe' -ArgumentList /q, /norestart
 if (($null -ne $process.ExitCode) -and ($process.ExitCode -ne 0)) { Write-Host ('EXIT CODE '+$process.ExitCode) } else { Write-Host 'Success' }
