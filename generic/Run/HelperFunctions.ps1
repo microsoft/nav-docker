b649ff7f2d98e988c59ea878b2125fde9b8058a4
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2024-09-23 11:52:03 +0200

    include AItest (#567)
    Co-authored-by: freddydk <freddydk@users.noreply.github.com>

--- generic/Run/HelperFunctions.ps1
+++ generic/Run/HelperFunctions.ps1
@@ -798,7 +798,7 @@ function GetTestToolkitApps {
 
         if (!$includeTestFrameworkOnly) {
             # Add Test Libraries
-            $apps += "Microsoft_System Application Test Library.app", "Microsoft_Business Foundation Test Libraries.app", "Microsoft_Tests-TestLibraries.app" | ForEach-Object {
+            $apps += "Microsoft_System Application Test Library.app", "Microsoft_Business Foundation Test Libraries.app", "Microsoft_Tests-TestLibraries.app", "Microsoft_AI Test Toolkit" | ForEach-Object {
                 @(get-childitem -Path "C:\Applications\*.*" -recurse -filter $_)
             }
 
