125770adc4e6fe597f8fff3f877f8633d791cec3
Maria Zhelezova <mazhelez@microsoft.com>
2025-06-16 10:48:15 +0200

    Support SQL server express 2022

--- generic/Run/SetupUrls.ps1
+++ generic/Run/SetupUrls.ps1
@@ -1,10 +1,10 @@
-$sql2019url = 'https://aka.ms/bcdocker-Sql2019Url'
+$sql2022url = 'https://aka.ms/bcdocker-Sql2022Url'
 
 # https://learn.microsoft.com/en-us/troubleshoot/sql/releases/download-and-install-latest-updates#latest-updates-available-for-currently-supported-versions-of-sql-server
 # Click the link under latest cumulative update including the latest GDR update (NOT the link under latest GDR)
-# In the KB article, click Method 3: Microsoft Download Center -> Download Pakcage now -> Download and right click "click here to download manually" -> Copy link address
+# In the KB article, look for "How to obtain and install the update", click Method 3: Microsoft Download Center -> Download Package now -> Download and right click "click here to download manually" -> Copy link address
 # The file is around 900Mb (GDR update alone is smaller)
-$sql2019LatestCuUrl = 'https://aka.ms/bcdocker-Sql2019LatestCuUrl'
+$sql2022LatestCuUrl = 'https://aka.ms/bcdocker-Sql2022LatestCuUrl'
 
 # https://dotnet.microsoft.com/en-us/download/dotnet/6.0 - grab the direct link behind ASP.NET Core Runtime Windows -> Hosting Bundle
 $dotNet6url = 'https://aka.ms/bcdocker-DotNet6Url'
